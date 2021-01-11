595e5fd50f0a975ddf86d10e1140d12feecaa6e4
Park, JeeHyun <jhyun19@gmail.com>
2021-01-11 01:57:32 -0600

    code
    python codes to run mat functions

--- python/gen_dataset_case9_n_2.py
+++ python/gen_dataset_case9_n_2.py
@@ -1,5 +1,6 @@
 import matlab.engine
 import os, glob
+from tqdm import trange
 
 # generate the dataset by COSMIC
 print("> Contingency: N-2")
@@ -14,7 +15,7 @@ num_branches = 9
 
 print("> Number of Branches: {}".format(num_branches))
 
-for i in range(1, num_branches + 1):
+for i in trange(1, num_branches + 1):
     for j in range(1, num_branches + 1):
         if i == j:
             pass
@@ -22,7 +23,7 @@ for i in range(1, num_branches + 1):
             print()
             print("- tripped branch index: {} & {}".format(i, j))
             print()
-            eng.sim_case9_n_2(i, j, False)
+            eng.sim_case9_n_2(i, j, "../results/case9/", False)
 
             for filename in glob.glob("../results/case9/sim*"):
                 os.remove(filename)
